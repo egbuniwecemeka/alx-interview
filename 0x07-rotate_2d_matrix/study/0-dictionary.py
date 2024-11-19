32f085366f17c30b664b58cb2feebb236ee3a0a4
egbuniwecemeka <egbuniwecemeka.com>
2024-11-19 16:07:20 +0100

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/study/0-dictionary.py
+++ 0x07-rotate_2d_matrix/study/0-dictionary.py
@@ -25,3 +25,9 @@ print(test_membership)
 # test non-membership of key in dictionary
 non_membership = 'lawal' not in sort
 print(non_membership)
+
+# dict() builds a dictionary from key:value pairs sequences
+create_dict = dict([('a', 1), ('b', 2), ('c', 3)])
+print(create_dict)
+
+# Looping techniques - the items() method can be used in retrieving keys and methods
