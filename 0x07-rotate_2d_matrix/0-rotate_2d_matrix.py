8164a5ac9708cb7d5a4d7ce61d51c33ed5e1f621
egbuniwecemeka <egbuniwecemeka.com>
2024-11-20 17:10:08 +0100

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,10 +2,12 @@
 """Rotate a 2D Matrix in a anti-clockwise direction"""
 
 # Naive approach - O(n^2) Time and O(n^2) Space
-# This includes moving row1/first row to first column in reverse order and so on.
+# This includes moving row1/first row to first column in
+# reverse order and so on.
 # In the matrix above a (3*3) where n=3
 # Example matrix[0][0] will become matrix[2][0]
 # Example matrix[2][2] should be matrix[0][2]
+
 
 def rotate_2d_matrix(matrix):
     """Rotates a n by n 2D matrix 90degrees anti-clockwise"""
