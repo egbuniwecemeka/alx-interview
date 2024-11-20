8766ff478cd62ca6e0db850279cc4e73c85882d5
egbuniwecemeka <egbuniwecemeka.com>
2024-11-20 16:45:07 +0100

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/rotate_matrix.py
+++ 0x07-rotate_2d_matrix/rotate_matrix.py
@@ -16,7 +16,7 @@ def rotate_2d_matrix(matrix):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    for i in len(n):
+    for i in range(n):
         matrix[i].reverse()
 
     print(matrix)
