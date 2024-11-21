d2d90fd0e8c1ca265c3fdcb5772d126281dae72b
egbuniwecemeka <egbuniwecemeka.com>
2024-11-21 15:31:16 +0100

    2D matrix rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,15 +6,13 @@
 
 def rotate_2d_matrix(matrix):
     """Rotates a n by n 2D matrix 90degrees anti-clockwise"""
+    n  = len(matrix)
 
-    # there are n/2 squares or cycles in a matrix of side n. Run a loop to traverse the matrix a cycle at a time.
-    n = len(matrix)
-
-    # Tranverse the matrix a cycle at a time
-    for i in range(n//2):
-        for j in range(i, n - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[j][n - 1 - i]
-            matrix[j][n - 1 - i] = matrix[n - 1 - i][n - 1 - j]
-            matrix[n - 1 - i][n - 1 - j] = matrix[n - 1 - j][i]
-            matrix[n - 1 - j][i] = temp
+    # Reverse the rows
+    for row in matrix:
+        row.reverse()
+    
+    # Transposing rows and colums
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
