91baf54948c9e8367d03a8206e85ee17a7a667d6
egbuniwecemeka <egbuniwecemeka.com>
2024-11-21 14:12:11 +0100

    2D matrix rotation

--- 0x07-rotate_2d_matrix/study/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/study/0-rotate_2d_matrix.py
@@ -51,7 +51,8 @@ def expected_2d_rotate(mat):
 if __name__ == "__main__":
     matrix = [[1, 2, 3, 4],
               [5, 6, 7, 8],
-              [9, 10, 11, 12]
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]
               ]
     
     rotate_2d_matrix(matrix)
