a59cf82f0ca9ef1edcf2d475f804239f48be99b0
egbuniwecemeka <egbuniwecemeka.com>
2024-11-21 15:38:53 +0100

    2D matrix rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,10 @@
 
 def rotate_2d_matrix(matrix):
     """Rotates a n by n 2D matrix 90degrees anti-clockwise"""
-    n  = len(matrix)
 
-    # Reverse the rows
-    for row in matrix:
-        row.reverse()
+    matrix.reverse()
+
+    n  = len(matrix)
     
     # Transposing rows and colums
     for i in range(n):
