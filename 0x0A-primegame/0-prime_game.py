0be1c07bcec738b01a6a66f60af7a4c0386e2eb2
egbuniwecemeka <egbuniwecemeka.com>
2024-12-12 22:32:16 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python3
-"""A script """
-from typing import List
-from math import sqrt
+"""A script that selects the winner of a game of prime numbers"""
 
-def isPrime(nums: List[int]) -> List[int]:
-    """ Returns a list of prime numbers from a list of integers
-        Args:
-            nums - A list of integers
-        Returns:
-            A list of prime numbers
+def isWinner(x, nums):
     """
-    def isprime(n: int) -> bool:
-        """Checks if a nmber is prime or even"""
-        if n <= 0:
-            print('Input a positive integer')
-        
-        for i in range(2, int(sqrt(n)) + 1):
-            if n % i == 0:
-                return False
-        return True
-    
-    return [num for num in nums if isprime(num)]
-    
+    Determine the winner of a game of prime numbers
 
-if __name__ == "__main__":
-    array = [1, 3, 4, 5, 7, 9]
-    res = isPrime(array)
-    print(f'Result: {res}')
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of integers
+    
+    Returns:
+        str or None: Name of the winner ('Maria' or 'Ben' or None if tied)
+    """
+    if x < 1 or not nums:
+        return None
+    
+    # Find the maxium number in nums for prime sieve
+    max_num = max(nums)
+
+    # Generate a list of primes using Sieve of Eratosthenes
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False # 0 and 1 are not primes
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[1]:
+            for multiple in range(i * i, max_num + 1, i):
+                is_prime[multiple] = False
+    
+    # Precompute the number of primes up to each number
+    primes_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        primes_count[i] = primes_count[i - 1] + (1 if is_prime[i] else 0)
+
+    # Determine winners for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if primes_count[n] % 2 == 0: # Ben wins if the count is even
+            ben_wins += 1
+        else: # Maria wins if the count is odd
+            maria_wins += 1
+    
+    if maria_wins > ben_wins:
+        return 'Maria'
+    elif ben_wins > maria_wins:
+        return 'Ben'
+    else:
+        return None
